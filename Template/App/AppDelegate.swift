cad6747deb724865da57c6a60d84e7bc8f11bbec
Bassist_Zero <bassist.zero@icloud.com>
2023-03-06 16:25:59 +0300

    Less logo screen time

--- Template/App/AppDelegate.swift
+++ Template/App/AppDelegate.swift
@@ -6,6 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 0.5)
         launchApp()
         return true
     }
