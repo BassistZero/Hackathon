d9d751ec7cd8f35f01410d7f7b3de1686204eab9
Bassist_Zero <bassist.zero@icloud.com>
2023-03-06 16:29:22 +0300

    Add error handling for manual bar-coding

--- Template/Modules/Manual/ManualViewController.swift
+++ Template/Modules/Manual/ManualViewController.swift
@@ -4,8 +4,13 @@ final class ManualViewController: UIViewController {
 
     // MARK: - Private Outlets
 
+    @IBOutlet private weak var messageLabel: UILabel!
     @IBOutlet private weak var barCodeTextField: UITextField!
     @IBOutlet private weak var requestInfoButton: UIButton!
+
+    // MARK: - Private Properties
+
+    private var barCodeLength = 13
 
     // MARK: - UIViewController
 
@@ -13,6 +18,7 @@ final class ManualViewController: UIViewController {
         super.viewDidLoad()
         setupInitialState()
     }
+
 }
 
 // MARK: - Configuration
@@ -21,7 +27,53 @@ private extension ManualViewController {
 
     func setupInitialState() {
         requestInfoButton.setTitle(.localized(key: "RequestData"), for: .normal)
-        barCodeTextField.placeholder = "4 627081 051484"
+        requestInfoButton.isEnabled = false
+
+        barCodeTextField.placeholder = .localized(key: "Manual.placeholder")
+        barCodeTextField.delegate = self
+
+
+        configureLabels()
+    }
+
+    func configureLabels() {
+        messageLabel.numberOfLines = 0
+        messageLabel.font = .systemFont(ofSize: 12)
+        configureMessageText()
+    }
+
+    func configureMessageText() {
+        guard let text = barCodeTextField.text, !text.isEmpty else {
+            messageLabel.text = ""
+            return
+        }
+
+        if text.count < barCodeLength {
+            messageLabel.textColor = .systemRed
+            var amountText: String
+
+            switch barCodeLength - text.count {
+            case 1:
+                amountText = "цифру"
+            case 2,3,4:
+                amountText = "цифры"
+            default:
+                amountText = "цифр"
+            }
+
+            messageLabel.text = "Введите ещё \(barCodeLength - text.count) \(amountText)"
+        }
+
+        if text.count == barCodeLength {
+            messageLabel.textColor = .systemGreen
+            messageLabel.text = "Нажмите 'запросить'"
+        }
+    }
+
+    func configureRequestButton() {
+        guard let text = barCodeTextField.text else { return }
+
+        requestInfoButton.isEnabled = text.count == barCodeLength ? true : false
     }
 
 }
@@ -31,13 +83,37 @@ private extension ManualViewController {
 private extension ManualViewController {
 
     @IBAction func sendBarCode(_ sender: UIButton) {
-        // TODO: Show Detail Screen
+        let controller = DetailViewController()
+        guard let barCode = barCodeTextField.text else { return }
+
+        LocalStorage.appendBarCode(barCode)
+
+        controller.barCode = barCode
+        present(controller, animated: true)
     }
 
 }
 
-// MARK: - Private Methods
+// MARK: - UITextFieldDelegate
 
-private extension ManualViewController {
+extension ManualViewController: UITextFieldDelegate {
+
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        configureMessageText()
+        configureRequestButton()
+        guard let text = textField.text else { return }
+
+        if text.count > barCodeLength {
+            textField.deleteBackward()
+        }
+
+        print("textFieldDidChangeSelection")
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+    }
 
 }
